9db262b2c136c4a0919a6b2b86e847515bc1f745
Patrick K <patrickkim93@gmail.com>
2016-10-12 15:52:16 +0900

    Add files via upload

---
+++ JS%20Design%20Patterns/StructuralPattern/flyweight/flyweight.js
@@ -0,0 +1,5 @@
+//Conserve memory by sharing portions of an object and sharing it between objs
+//Tasks had lots of non unique data
+//Example: Thousands of tasks, has same data. Flyweight helps reduce this usage
+//Like a boxer --- a flyweight
+//Only useful if you have large numbers of objects
